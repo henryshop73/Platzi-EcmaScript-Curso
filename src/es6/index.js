51b1768f2172f6b1f3b94747ed2ab061997816c2
henryshop73 <henrymoranfreelance@gmail.com>
2022-07-11 19:02:35 -0500

    Primer commit agregando los archivos del curso

---
+++ src/es6/index.js
@@ -0,0 +1,84 @@
+function newfunction(name, age, country) {
+    var name = name || 'Oscar';
+    var age = age || 32;
+    var country = country || 'MX';
+    console.log(name, age, country);
+}
+
+// es6
+
+function newfunction2(name = 'oscar', age = 32, country = 'MX') {
+    console.log(name, age, country);
+};
+
+newfunction2();
+newfunction2('Ricardo', '23', 'CO');
+
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+let epicPhrase2 = `${hello} ${world}`; //Ahora con em6 se puede concatenar elementos con comillas invertidas
+console.log(epicPhrase2);
+
+// Multilínea en los string
+
+// - Antes se hacía así:
+
+let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
++ "Otra frase épica que necesitamos."
+
+// - es6
+
+let lorem2 = `otra frase épica que necesitamos
+ahora es otra frase épica
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+// Destructuración de elementos
+
+let person = {
+    'name': 'oscar',
+    'age': '32',
+    'country': 'MX'
+}
+
+// - Antes
+console.log(person.name, person.age);
+
+// - es6
+let { name, age } = person;
+console.log(name, age);
+
+
+// Spread Operator
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = [ 'Valeria', 'Jessica', 'Camila'];
+
+let education = [ 'David', ...team1, ...team2];
+
+console.log(education);
+
+
+// Var (global vs Let(scope))
+
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = "Global Let";
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+// console.log(globalLet); No podemos acceder . Tenemos que colocar el console.log dentro del scope de let
+
+//Const
+// - No se puede easignar un valor a una constante
+
+const a = "b";
+a = "a"; // No se puede reasignar este valora a
