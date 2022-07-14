8b3510473ca933953c9405941b516e981e8ec52d
henryshop73 <henrymoranfreelance@gmail.com>
2022-07-13 20:27:14 -0500

    Agregando ejemplos de Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -35,3 +35,31 @@ console.log('food'.padEnd(12, '-----'));
 const obj = {
     name: 'Oscar',
 }
+
+// Async Await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ?setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch (error) {
+        console.log(erroR);
+    }
+};
+
+anotherFunction();
